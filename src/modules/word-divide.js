56b15beeb163fae7a8bf49f19774c17fb20dcddc
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-29 00:12:04 +0400

    refactor: word-divide function

--- src/modules/word-divide.js
+++ src/modules/word-divide.js
@@ -1,64 +1,25 @@
-const findStressPosition = require('./find-stress-position');
 const findVowelsNums = require('./find-vowels-nums');
-const getHandledWord = require('./get-handled-word');
 
-/* разбивает слово на слоги */
 const wordDivide = word => {
-  // эта штука добавит слову ударений
-  const wordWithStress = getHandledWord(word);
+  const matchedWord = word.toLowerCase();
+  const vowelsNums = findVowelsNums(matchedWord);
 
-  const vowelsNums = findVowelsNums(wordWithStress);
-  const syllables = [];
-
-  // если состоит из одного слога, то этот слог всё слово
-  if (vowelsNums.length <= 1) {
-    syllables.push(wordWithStress);
-    return syllables;
-  }
-
-  const stressPos = findStressPosition(wordWithStress);
-
-  // переменные, которые надо заранее определить
-  let newStart = 0;
-  let previousStep = 0;
-
-  for (let i = 0; i < vowelsNums.length; i++) {
-    let syllable = '';
-    let step = 1;
-    /* наличие ударения увеличивает шаг, шаг - это количество
-		букв, которое надо взять после текущей гласной в слог*/
-    if (stressPos > vowelsNums[i] && stressPos < vowelsNums[i + 1]) {
-      step++;
-    }
-    // шаг уменьшается, если рядом гласная, чтобы её не захватить
-    if (vowelsNums[i + 1] - vowelsNums[i] <= step + 1) {
-      step--;
-    }
-    // формируем слоги
-    if (i === 0) {
-      /* первый слог*/ for (let j = 0; j <= vowelsNums[i] + step; j++) {
-        syllable += wordWithStress[j];
-      }
-    } else if (i === vowelsNums.length - 1) {
-      /* последний слог*/ for (
-        let j = newStart;
-        j <= wordWithStress.length - 1;
-        j++
-      ) {
-        syllable += wordWithStress[j];
-      }
-    } else {
-      for (let j = newStart; j <= vowelsNums[i] + step; j++) /* остальные*/ {
-        syllable += wordWithStress[j];
-      }
-    }
-    syllables.push(syllable);
-    // шаг предыдущего слога
-    previousStep = step;
-    // начало нового слога
-    newStart = vowelsNums[i] + previousStep + 1;
-  }
-  return syllables;
+  return vowelsNums
+    .map((vowel, index, array) => {
+      const previousVowel = array[index - 1];
+      if (previousVowel === undefined) return 0;
+      if (vowel - previousVowel === 1) return vowel;
+      if (vowel - previousVowel === 2) return previousVowel + 1;
+      return previousVowel + 2;
+    })
+    .map((start, index, array) => {
+      const end =
+        array[index + 1] !== undefined
+          ? array[index + 1] - 1
+          : matchedWord.length - 1;
+      return [start, end];
+    })
+    .map(([start, end]) => matchedWord.slice(start, end + 1));
 };
 
 module.exports = wordDivide;
