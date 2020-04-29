44e7e002893388f23e6ce47bd36a2651e0d71c4f
Alexander Fraltsov <kolonessens@yandex.ru>
2020-04-29 17:31:09 +0400

    refactor: score words function

--- src/modules/score-words.js
+++ src/modules/score-words.js
@@ -3,55 +3,112 @@ const findSyllableWithStress = require('./find-syllable-with-stress');
 const scoreSyllables = require('./score-syllables');
 const findStressPosition = require('./find-stress-position');
 
-// начисляет очки за совпадение слов
-const scoreWords = (word1, word2) => {
+const MULTIPLIER_STRESSED_SYLLABLE = 5;
+const MULTIPLIER_EXCESS_SYLLABLES = 2;
+const MULTIPLIER_EXCESS_LETTERS = 0.25;
+
+const findEnding = word => word.slice(findStressPosition(word) + 1);
+
+const findEndingLengthsDifference = (word1, word2) =>
+  Math.abs(findEnding(word1).length - findEnding(word2).length);
+
+const findSyllablesQuantityDifference = (word1, word2) =>
+  Math.abs(wordDivide(word1).length - wordDivide(word2).length);
+
+const getScoresForNonStressedSyllables = (
+  slblsMany,
+  stressedManyPos,
+  slblsFew,
+  stressedFewPos,
+  isSyllablesBefore
+) => {
   let score = 0;
-
-  const syllables1 = wordDivide(word1);
-  const syllables2 = wordDivide(word2);
-  const l1 = syllables1.length;
-  const l2 = syllables2.length;
-
-  // ударные слоги
-  const stressedSyl1Pos = findSyllableWithStress(word1) - 1;
-  const stressedSyl2Pos = findSyllableWithStress(word2) - 1;
-  const delta = stressedSyl1Pos - stressedSyl2Pos;
-
-  // сравниваем ударные слоги (увеличивающий коэффициент)
-  score +=
-    5 *
-    scoreSyllables(syllables1[stressedSyl1Pos], syllables2[stressedSyl2Pos]);
-
-  // сравниваем слоги после (!!цикл для большего количества слогов)
-  if (l1 - stressedSyl1Pos >= l2 - stressedSyl2Pos) {
-    for (let i = stressedSyl1Pos + 1; i < l1; i++) {
-      score += scoreSyllables(syllables1[i], syllables2[i - delta] || '');
-    }
+  if (isSyllablesBefore) {
+    slblsMany
+      .filter((el, index) => index < stressedManyPos)
+      .reverse()
+      .forEach((syl, index) => {
+        score += scoreSyllables(syl, slblsFew[stressedFewPos - 1 - index]);
+      });
   } else {
-    for (let i = stressedSyl2Pos + 1; i < l2; i++) {
-      score += scoreSyllables(syllables1[i + delta] || '', syllables2[i]);
-    }
+    slblsMany
+      .filter((el, index) => index > stressedManyPos)
+      .forEach((syl, index) => {
+        score += scoreSyllables(syl, slblsFew[stressedFewPos + 1 + index]);
+      });
   }
-  // сравниваем слоги до
-  if (stressedSyl1Pos >= stressedSyl2Pos) {
-    for (let i = stressedSyl1Pos - 1; i >= 0; i--) {
-      score += scoreSyllables(syllables1[i], syllables2[i - delta] || '');
-    }
-  } else {
-    for (let i = stressedSyl2Pos - 1; i >= 0; i--) {
-      score += scoreSyllables(syllables1[i + delta] || '', syllables2[i]);
-    }
-  }
-
-  // штрафуем за количество слогов
-  score -= Math.abs(l1 - l2) * 2;
-
-  // штрафуем за лишние(недостающие) буквы после гласной в последнем слоге
-  const ending1 = word1.slice(findStressPosition(word1) + 1);
-  const ending2 = word2.slice(findStressPosition(word2) + 1);
-  score -= Math.abs(ending1.length - ending2.length) * 0.25;
-
   return score;
 };
+
+const scoreOtherSylls = (word1, word2) => {
+  const sylls1 = wordDivide(word1);
+  const sylls2 = wordDivide(word2);
+
+  const positionStressedSyl1 = findSyllableWithStress(word1) - 1;
+  const positionStressedSyl2 = findSyllableWithStress(word2) - 1;
+
+  let score = 0;
+
+  if (
+    sylls1.length - sylls2.length >=
+    positionStressedSyl1 - positionStressedSyl2
+  ) {
+    score += getScoresForNonStressedSyllables(
+      sylls1,
+      positionStressedSyl1,
+      sylls2,
+      positionStressedSyl2,
+      false
+    );
+  } else {
+    score += getScoresForNonStressedSyllables(
+      sylls2,
+      positionStressedSyl2,
+      sylls1,
+      positionStressedSyl1,
+      false
+    );
+  }
+
+  if (positionStressedSyl1 >= positionStressedSyl2) {
+    score += getScoresForNonStressedSyllables(
+      sylls1,
+      positionStressedSyl1,
+      sylls2,
+      positionStressedSyl2,
+      true
+    );
+  } else {
+    score += getScoresForNonStressedSyllables(
+      sylls2,
+      positionStressedSyl2,
+      sylls1,
+      positionStressedSyl1,
+      true
+    );
+  }
+  return score;
+};
+
+const scoreStressedSylls = (word1, word2) => {
+  const syllables1 = wordDivide(word1);
+  const syllables2 = wordDivide(word2);
+
+  const stressedSylPos1 = findSyllableWithStress(word1) - 1;
+  const stressedSylPos2 = findSyllableWithStress(word2) - 1;
+
+  const stressedSyl1 = syllables1[stressedSylPos1];
+  const stressedSyl2 = syllables2[stressedSylPos2];
+
+  return (
+    MULTIPLIER_STRESSED_SYLLABLE * scoreSyllables(stressedSyl1, stressedSyl2)
+  );
+};
+
+const scoreWords = (word1, word2) =>
+  scoreStressedSylls(word1, word2) +
+  scoreOtherSylls(word1, word2) -
+  findSyllablesQuantityDifference(word1, word2) * MULTIPLIER_EXCESS_SYLLABLES -
+  findEndingLengthsDifference(word1, word2) * MULTIPLIER_EXCESS_LETTERS;
 
 module.exports = scoreWords;
